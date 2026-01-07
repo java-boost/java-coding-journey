ad3dd2a45a4998e316e80b9b637953f4635be306
java-boost <vamsikrishnas2004@yahoo.com>
2026-01-07 09:27:32 -0500

    Learning data types
    Initial java learning check in for data types

---
+++ src/main/java/User.java
@@ -0,0 +1,38 @@
+public class User {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
